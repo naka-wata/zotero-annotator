599bb9f9a6bf3764bb8551b204bdaed639a7ab7b
Wataru Nakamura <watarunakamura@WatarunoMacBook-Pro.local>
2026-03-29 00:33:23 +0900

    refactor: extract ItemResult and TranslationItemResult to models/results.py

---
+++ src/zotero_annotator/models/__init__.py
@@ -0,0 +1,13 @@
+from zotero_annotator.models.results import (
+    ItemResult,
+    TranslationItemResult,
+    make_skipped_item_result,
+    make_skipped_translation_result,
+)
+
+__all__ = [
+    "ItemResult",
+    "TranslationItemResult",
+    "make_skipped_item_result",
+    "make_skipped_translation_result",
+]
