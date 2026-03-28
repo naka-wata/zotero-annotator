459112a84169bd18f54573fb2a07b3690c8f2fb0
Wataru Nakamura <watarunakamura@WatarunoMacBook-Pro.local>
2026-03-29 00:38:28 +0900

    feat: add ZoteroItem/ZoteroAnnotation TypedDict definitions to models/zotero.py

--- src/zotero_annotator/models/__init__.py
+++ src/zotero_annotator/models/__init__.py
@@ -4,10 +4,22 @@ from zotero_annotator.models.results import (
     make_skipped_item_result,
     make_skipped_translation_result,
 )
+from zotero_annotator.models.zotero import (
+    ZoteroAnnotation,
+    ZoteroAnnotationData,
+    ZoteroItem,
+    ZoteroItemData,
+    ZoteroTag,
+)
 
 __all__ = [
     "ItemResult",
     "TranslationItemResult",
     "make_skipped_item_result",
     "make_skipped_translation_result",
+    "ZoteroAnnotation",
+    "ZoteroAnnotationData",
+    "ZoteroItem",
+    "ZoteroItemData",
+    "ZoteroTag",
 ]
